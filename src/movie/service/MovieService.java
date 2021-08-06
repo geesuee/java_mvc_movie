9776f2335e327210b96d9256dea785ff558d222d
Bae Jisu <jjoo4956@hanyang.ac.kr>
2021-08-06 20:29:00 +0900

    modify reservationUpdate

--- src/movie/service/MovieService.java
+++ src/movie/service/MovieService.java
@@ -148,10 +148,10 @@ public class MovieService {
 		if(!mappedMovieTitle.equals(newReservation.getMovieTitle())) {
 			throw new NotExistException("입력하신 영화는 해당 상영관에서 상영하지 않습니다.");
 		}
-		else if (reservation != null) {
+		if (reservation != null) {
 			throw new DuplicateException("해당 예약 번호와 동일한 번호의 예약 내역이 존재합니다.");
 		}
-		else if(theater.getRemainSeats() == 0) {
+		if(theater.getRemainSeats() == 0) {
 			throw new NotExistException("해당 상영관에 잔여 좌석이 없습니다.");
 		}
 		movieModel.insertReservation(newReservation);
@@ -162,10 +162,11 @@ public class MovieService {
 	
 	/**
 	 * Reservation의 영화 <상영관 번호, 영화 제목> 수정 - 예약자 이름으로 검색해서 해당 예약의 <상영관 번호, 영화 제목> 수정
-	 * 1. 상영관 번호, 영화 제목이 movieInfo에 매핑되어 있지 않을 경우(=해당 상영관에서 입력한 영화를 상영하지 않을 경우) NotExistException 발생
-	 * 2. 수정하고자 하는 예약이 존재하지 않을 경우 NotExistException 발생
+	 * 1. 수정하고자 하는 예약이 존재하지 않을 경우, NotExistException 발생
+	 * 2. 상영관 번호, 영화 제목이 movieInfo에 매핑되어 있지 않을 경우,(=해당 상영관에서 입력한 영화를 상영하지 않을 경우) NotExistException 발생
+	 * 3. 해당 상영관에 잔여 좌석이 없을 경우, NotExistException 예외 발생
 	 * 
-	 * 3. 수정 성공 시
+	 * 4. 수정 성공 시
 	 * 		- 이전 상영관의 예약 좌석 수-1;
 	 * 		- 새로운 Reservation 상영관의 예약 좌석 수+1;
 	 * 
@@ -180,11 +181,14 @@ public class MovieService {
 		
 		Reservation reservation = getReservation(name);
 		
+		if(reservation == null){
+			throw new NotExistException("수정하고자 하는 예약 내역이 존재하지 않는 에러가 발생했습니다.");
+		}
 		if(!mappedMovieTitle.equals(movieTitle)) {
 			throw new NotExistException("입력하신 영화는 해당 상영관에서 상영하지 않습니다.");
 		}
-		if(reservation == null){
-			throw new NotExistException("수정하고자 하는 예약 내역이 존재하지 않는 에러가 발생했습니다.");
+		if(newTheater.getRemainSeats() == 0) {
+			throw new NotExistException("해당 상영관에 잔여 좌석이 없습니다.");
 		}
 		
 		// 이전 상영관의 예약 좌석 수-1;
